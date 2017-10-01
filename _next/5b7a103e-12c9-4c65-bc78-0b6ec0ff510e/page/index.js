adc36ab066d22d3ca435b5bcca84ee0fdfffb3c8
Mahatthana Nomsawadi via CircleCI <me@codekelvin.com>
2017-10-01 11:01:12 +0000

    Deploy [ci skip]

--- _next/5b7a103e-12c9-4c65-bc78-0b6ec0ff510e/page/index.js
+++ _next/5b7a103e-12c9-4c65-bc78-0b6ec0ff510e/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[379]);
+            var comp = module.exports=webpackJsonp([3],[],[230]);
             return { page: comp.default }
           })
         
